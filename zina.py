c459c05461596990686154c4d00d70244939f64c
daryabu <133571574+daryabu@users.noreply.github.com>
2023-05-14 19:15:37 +0700

    Add files via upload

--- zina.py
+++ zina.py
@@ -556,7 +556,7 @@ def calculate(*args: tuple):
         num_1, num_2 = int((list_of_nums[-3]).strip()), int((list_of_nums[-1]).strip())
         opers = [list_of_nums[0].strip(), list_of_nums[-2].strip()]
         for i in opers:
-            if 'дел' in i or 'множ' in i or 'лож' in i or 'приба' in i or 'выч' in i or i == 'x' or i == 'х' or i == '/' or i == '+' or i == '-' or i == '*':
+            if 'дел' in i or 'множ' in i or 'лож' in i or 'приба' in i or 'выч' in i or i == 'x' or i == 'х' or i == '+' or i == '/' or i == '-' or i == '*':
                 oper = i
                 break
             else:
@@ -565,7 +565,7 @@ def calculate(*args: tuple):
             ans = num_1 + num_2
         elif oper == "-" or 'выче' in oper:
             ans = num_1 - num_2
-        elif oper == "х" or 'множ' or 'x' or '*' in oper:
+        elif oper == "х" or oper == 'x' or 'множ' in oper or oper == '*':
             ans = num_1 * num_2
         elif oper == "/" or 'дел' in oper:
             if num_2 != 0:
@@ -574,7 +574,7 @@ def calculate(*args: tuple):
                 play_voice_assistant_speech(translator.get("Division by zero!"))
         elif "степен" in oper:
             ans = num_1 ** num_2
-        play_voice_assistant_speech("{0} {1} {2} = {3}".format(list_of_nums[-3], list_of_nums[-2], list_of_nums[-1], ans))
+        play_voice_assistant_speech("{0} {1} {2} = {3}".format(list_of_nums[-3], list_of_nums[-2], list_of_nums[-1], round(ans,2)))
     except:
         play_voice_assistant_speech(translator.get("Say, for example: Count 5+5"))
 
